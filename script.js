c876f93a9abf7fe66bbccb75b0fb3ece754f00bc
Tony Pham <tony.pham@pch.gc.ca>
2021-02-08 11:29:06 -0500

    Add modal content for scheduling reports appearing below schedule button on click

---
+++ script.js
@@ -0,0 +1,6 @@
+var scheduleModal = document.getElementById("schedule-modal");
+var scheduleBtn = document.getElementById("schedule-btn");
+
+scheduleBtn.onclick = () => {
+    scheduleModal.style.display = "block";
+}
